b25dc37e67e06bab565aacf38cd83e3e54e44a29
Juan Manetta <Jmanetta@frsn.utn.edu.ar>
2024-06-24 19:16:07 -0300

    feat: Add chat functionality and styling
    This commit adds chat functionality and styling to the web application. The changes include:
    - Adding a link to the style.css file in the index.html file
    - Modifying the main.js file to include chat functionality using the genAI library
    - Updating the style.css file to remove unnecessary styles and add styling for the chat messages

--- main.js
+++ main.js
@@ -6,30 +6,42 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 
 const formEl = document.getElementById("form");
 
-let prompt = String.Empty;
+const promptPlus = "\n Escribe siempre con acento argentino, No uses emojies";
+
+let promptOriginal = String.Empty;
+let prompt = promptOriginal;
+  
+const chat = model.startChat({
+  history: [],
+});
 
 document.addEventListener('DOMContentLoaded', function() {
 
   formEl.addEventListener('submit', function(event){
     event.preventDefault();
-    prompt = document.getElementById("input").value;
+    promptOriginal = document.getElementById("input").value;
+    prompt = promptOriginal + promptPlus;
     run();
     
   })
 });
 
-
 async function run() {
- 
-
-  const result = await model.generateContent(prompt);
+  let text = "";
+  const result = await chat.sendMessage(prompt);
   const response = result.response;
-  const text = response.text();
+  text = response.text();
   console.log(text);
-  const divEl = document.getElementById("respuesta-div");
-  const nuevoParrafo = document.createElement('p');
-  nuevoParrafo.innerText = text;
-  divEl.appendChild(nuevoParrafo);
+  const divEl = document.getElementById("respuestas");
+  const nuevoParrafoUser = document.createElement('p');
+  nuevoParrafoUser.classList.add('user');
+  const nuevoParrafoIA = document.createElement('p');
+  nuevoParrafoIA.classList.add('ia');
+  nuevoParrafoUser.innerText = promptOriginal;
+  nuevoParrafoIA.innerText = text;
+  divEl.appendChild(nuevoParrafoUser);
+  divEl.appendChild(nuevoParrafoIA);
+  console.log(history);
 }
 
 
